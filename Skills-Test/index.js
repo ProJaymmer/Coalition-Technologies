bd299352500dcbb4d442b976eb94c2d977225b44
jay.nguyen <jay.nguyen@smoothstack.com>
2022-08-02 16:56:35 -0700

    Added carousel.

--- Skills-Test/index.js
+++ Skills-Test/index.js
@@ -6,3 +6,22 @@ function toggleMenu() {
 		menu.style.display = 'flex';
 	}
 }
+
+let slider = tns({
+	container: '.my-slider',
+	slideBy: 1,
+	speed: 400,
+	// The dots showing which slide you're on.
+	nav: false,
+	// Change the default controls to the div id #controls.
+	controlsContainer: '#controls',
+	prevButton: '.previous',
+	nextButton: '.next',
+	responsive: {
+		// How many items to display contingent on the amount of pixels.
+		850: {
+			items: 4,
+			gutter: 20,
+		},
+	},
+});
